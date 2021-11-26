cd9e018df98ce1ac10316d261e3b8f0724d1c3f5
JohanAcaro <93380488+JohanAcaro@users.noreply.github.com>
2021-11-26 13:19:22 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,13 +6,28 @@ public class Main {
     Cliente cliente = new Cliente();
     
     cliente.setNombre("Indra");
-    System.out.println(cliente.getNombre);
+    System.out.println(cliente.getNombre());
     
-    cliente.setCiudad("Madrid");
-    System.out.println(cliente.getCiudad);
+    cliente.setCiudad("Sevilla");
+    System.out.println(cliente.getCiudad());
         
     cliente.setPresupuesto(10000);
-    System.out.println(cliente.getPresupuesto);
-        
+    System.out.println(cliente.getPresupuesto());
+    
+    System.out.println("El cliente "+cliente.getNombre()+"-"+cliente.getCiudad()+" tiene un presupuesto de "+cliente.getPresupuesto()+" euros");
+    
+    Cliente2 cliente2 = new Cliente2();
+    
+    cliente2.setNombre("Vass");
+    System.out.println(cliente2.getNombre());
+    
+    cliente2.setCiudad("Valencia");
+    System.out.println(cliente2.getCiudad());
+    
+    cliente2.setPresupuesto(12000);
+    System.out.println(cliente2.getPresupuesto());
+    
+        System.out.println("El cliente "+cliente2.getNombre()+"-"+cliente2.getCiudad()+" tiene un presupuesto de "+cliente2.getPresupuesto()+" euros");
+
     }
 }
