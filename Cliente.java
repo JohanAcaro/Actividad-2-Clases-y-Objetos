fa4bd3ec1ae98155d0c7e4e0b4a9be61bd1725ef
JohanAcaro <93380488+JohanAcaro@users.noreply.github.com>
2021-11-26 13:19:44 +0100

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -3,15 +3,24 @@ public class Cliente{
     public String ciudad;
     public int presupuesto;
     
-    
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
     public String getNombre(){
         return this.nombre;
+    }
+     
+    public void setCiudad(String ciudad){
+        this.ciudad=ciudad;
     }
     
     public String getCiudad(){
         return this.ciudad;
     }
-    
+     
+    public void setPresupuesto(int presupuesto){
+        this.presupuesto=presupuesto;
+    }
     public int getPresupuesto(){
         return this.presupuesto;
     }
