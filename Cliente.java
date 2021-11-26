19c0336d212cb43eb12fe800e5a0a346ff173c43
JohanAcaro <93380488+JohanAcaro@users.noreply.github.com>
2021-11-26 13:00:27 +0100

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,18 @@
+public class Cliente{
+    public String nombre;
+    public String ciudad;
+    public int presupuesto;
+    
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+    
+    public String getCiudad(){
+        return this.ciudad;
+    }
+    
+    public int getPresupuesto(){
+        return this.presupuesto;
+    }
+}
