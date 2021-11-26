1e98b137a0123fe8831b7913c966319cf4a00357
JohanAcaro <93380488+JohanAcaro@users.noreply.github.com>
2021-11-26 13:20:08 +0100

    Create Cliente2.java

---
+++ Cliente2.java
@@ -0,0 +1,27 @@
+public class Cliente2{
+    public String nombre;
+    public String ciudad;
+    public int presupuesto;
+    
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+     
+    public void setCiudad(String ciudad){
+        this.ciudad=ciudad;
+    }
+    
+    public String getCiudad(){
+        return this.ciudad;
+    }
+     
+    public void setPresupuesto(int presupuesto){
+        this.presupuesto=presupuesto;
+    }
+    public int getPresupuesto(){
+        return this.presupuesto;
+    }
+}
